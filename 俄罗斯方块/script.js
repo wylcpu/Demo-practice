dc6fee85c043720202b23e2a686d84e51d98fab2
wylcpu <wylcpu@163.com>
2015-05-13 11:31:11 +0800

    change

--- 俄罗斯方块/script.js
+++ 俄罗斯方块/script.js
@@ -1,24 +1,31 @@
+//游戏的耦合性太高了，思路不是很清楚，没有从整体上进行把握
 //现在假定每个小方格是30px，其中包括3px的边框
 //
 var canvas = document.getElementById("canvas");
+var cnavas2 = document.getElementById("canvas2");
+var context2 = canvas2.getContext("2d");
 var context = canvas.getContext("2d");
 canvas.width = 306;
 canvas.height = 635;
-canvas.style.margin = "0 400px";
+canvas2.width = 130;
+canvas2.height = 70;
 context.beginPath();
 context.rect(1,1,304,634);
 context.lineWidth = 3;
 context.strokeStyle = "green";
 context.stroke();
-context.translate(2,2)
-context.shadowBlur = 0;
+context.translate(2,2);
 var store = [];//储存每个放个的状态
 var flag = 1;//是否产生一个新的图形
 var shop = 1;//记录变形的数字
-var randNum = 0;
-var rand = 0;
-var timer = null;
-var score = 0;
+var timer = null;//定时器的控制
+var score = 0;//分数的显示
+var oldrand = 0;//记录当前显示移动的方块
+var newrand = 0;//记录下一个要实现的方块
+var smallarr = [[0,1,0,0,0,0,0],[0,0,2,0,0,0,0]];
+var stop = 1;
+//把这些都存放在一个对象中
+var oDate={};
 
 //创建一个21*10的方格，默认状态是0，即什么都没有
 //
@@ -39,51 +46,69 @@ var score = 0;
 // 
 
 for(var i=0; i<21; i++) {
-	var arr = [];
+	var ar = [];
 	for(var j=0; j<10; j++){
-		arr.push(0);
+		ar.push(0);
 	}
-	store.push(arr);
+	store.push(ar);
 }
-store2 = [[0,0,0,0],[0,0,0,0]];
-arr = null;
-console.log(store);
+ar = null;
 //绘制方格的函数
 //param posX 绘制开始的X坐标
 //param posY 绘制开始的Y坐标
 //param width 绘制的方格的宽度
 //param height 绘制的方格的高度
 //这个其实是可以加上颜色的参数的
-function drawRect(posX,posY,width,height) {
-	context.save();
-	context.translate(posX,posY);
-	context.beginPath();
-	context.rect(2,2,width-2,height-2);
-	context.closePath();
-	context.lineWidth = 4;
-	context.strokeStyle = "#000";
-	context.fillStyle = "#f00";
-	context.stroke();
-	context.fill();
-	context.restore();
+function drawRect(posX,posY,width,height,cxt) {
+	cxt.save();
+	cxt.translate(posX,posY);
+	cxt.beginPath();
+	cxt.rect(2,2,width-2,height-2);
+	cxt.closePath();
+	cxt.lineWidth = 4;
+	cxt.strokeStyle = "#000";
+	cxt.fillStyle = "#f00";
+	cxt.stroke();
+	cxt.fill();
+	cxt.restore();
 }
 //重新绘制整个画布
 //param arr 这个数组存放着要显示的方块
 //param cxt 绘图上下文
 function draw(arr,cxt) {
 	cxt.save();
-context.clearRect(0,0,302,632);
-context.fillStyle = "#EFEFEF";
-context.fillRect(0,0,302,632);
+cxt.clearRect(0,0,302,632);
+cxt.fillStyle = "#EFEFEF";
+cxt.fillRect(0,0,302,632);
 cxt.restore();
 for(var i=0; i<21; i++) {
 	for(var j=0; j<10; j++) {
 		if(arr[i][j] !== 0) {
-			drawRect(getPosX(i,j),getPosY(i,j),30,30);
+			drawRect(getPosX(i,j),getPosY(i,j),30,30,cxt);
 		}
 	}
 }
 }
+function smalldraw(cxt) {
+	cxt.save();
+	cxt.clearRect(0,0,130,70);
+	cxt.fillStyle = "#efefef";
+	cxt.fillRect(0,0,130,70);
+	cxt.restore();
+	for(var i=0; i<2; i++) {
+		for(var j=0;j<7; j++) {
+			if(smallarr[i][j] !== 0 && j>2) {
+				cxt.save();
+				cxt.translate(5,5);
+			drawRect(getPosX(i,j-3),getPosY(i,j-3),30,30,cxt);
+			cxt.restore();
+			}
+			smallarr[i][j] = 0;//绘制完进行清空；
+		}
+	}
+
+}
+
 //获取X坐标
 function getPosX(i,j) {
 	return 30*j;
@@ -102,7 +127,13 @@ function newGame() {
 	draw(store,context);
 timer = setInterval(function(){
 	if(flag) {
-		randRect(store);
+		oldrand = newrand;
+		newrand = ~~(Math.random()*7);//产生0-6的随机数
+		randRect(smallarr,newrand);
+		smalldraw(context2);
+		randRect(store,oldrand);
+		
+		shop =1;//每次产生新的方块时，进行初始化
 		flag = false;
 
 	}
@@ -112,55 +143,47 @@ timer = setInterval(function(){
 }
 //本函数是产生随机形状的方块
 //产生7种不同的形状
-function randRect(arr) {
+function randRect(arr,randNum) {
 	
-	shop =1;//每次产生新的方块时，进行初始化
-	rand = ~~(Math.random()*7);//产生0-6的随机数
-	switch(rand) {
+	
+	switch(randNum) {
 		case 0:
-		randNum = 0;
 		arr[0][3] = 2;
 		arr[0][4] = 2;
 		arr[0][5] = 2;
 		arr[1][3] = 2;
 		break;
 		case 1:
-		randNum = 1;
 		arr[0][3] = 2;
 		arr[0][4] = 2;
 		arr[0][5] = 2;
 		arr[1][5] = 2;
 		break;
 		case 2:
-		randNum = 2;
 		arr[0][3] = 2;
 		arr[0][4] = 2;
 		arr[0][5] = 2;
 		arr[1][4] = 2;
 		break;
 		case 3:
-		randNum = 3;
 		arr[0][3] = 2;
 		arr[0][4] = 2;
 		arr[0][5] = 2;
 		arr[0][6] = 2;
 		break;
 		case 4:
-		randNum = 4;
 		arr[0][4] = 2;
 		arr[0][5] = 2;
 		arr[1][3] = 2;
 		arr[1][4] = 2;
 		break;
 		case 5:
-		randNum = 5;
 		arr[0][3] = 2;
 		arr[0][4] = 2;
 		arr[1][3] = 2;
 		arr[1][4] = 2;
 		break;
 		case 6:
-		randNum = 6;
 		arr[0][3] = 2;
 		arr[0][4] = 2;
 		arr[1][4] = 2;
@@ -178,7 +201,7 @@ function checkDown(arr) {
 			if(arr[i][j] === 2) {//这里是否还要检查是否到底了？？？
 			if(i<20) {
 				if(arr[i+1][j] === 1) {
-					if(arr[1][j] === 1){
+					if(arr[2][j] === 1){
 						clearInterval(timer);
 						if(confirm("Are you again new game?")) {
 							newGame();
@@ -212,7 +235,8 @@ function heBing(arr) {
 	});
 	if(sum === 10) {
 		score =score + 10;
-		$("#grade").html("<span>" + score + "</span>");
+		document.getElementById("#grade").innerHTML = "<span>" + score
+		 + "</span>";//进行分数显示
 		for(var k = i-1; k>=0; k--) {
 			for(var j=0; j<10; j++) {
 				arr[k+1][j] = arr[k][j];
@@ -445,7 +469,7 @@ function bacomeOne(arr) {
  	break;
  	case 3:
  	if(shop === 1 || shop === 3) {
- 		for(var i=20; i>=0; i--) {
+ 		for(var i=20; i>=2; i--) {
  			for(var j=0; j<10; j++) {
  				if(arr[i][j] === 2) {
  					arr[i][j] = 0;
@@ -460,7 +484,7 @@ function bacomeOne(arr) {
  			}
  		}
  	} else {
- 		for(var i=20; i>=0; i--) {
+ 		for(var i=20; i>=2; i--) {
  			for(var j=0; j<10; j++) {
  				if(arr[i][j] === 2) {
  					if(j === 0 || j > 7) {
@@ -550,45 +574,72 @@ function bacomeOne(arr) {
  	break;
  	}
  } 
-newGame();
-document.onkeyup = function (evt) {
+newGame();//第一次开始时需要的
+document.onkeydown = function (evt) {
 	if(evt.keyCode === 38) {
 		if(shop === 4) {
 			shop = 0;
 		}
 		
-		shape(rand,store);	
+		shape(oldrand,store);	
 		
 	} else if(evt.keyCode === 37) {
 		moveLeft(store);
 	} else if (evt.keyCode === 39) {
 		moveRight(store);
+	} else if(evt.keyCode === 40) {
+		setTimeout(function() {//为了使运动不太快，也是为了平滑
+		moveDown(store);	
+		},30);
+
+	} else if(evt.keyCode === 32) {
+		if(stop) {
+			clearInterval(timer);
+			stop = 0;
+		} else {
+			stop = 1;
+			timer = setInterval(function(){
+				if(flag) {
+					oldrand = newrand;
+					newrand = ~~(Math.random()*7);//产生0-6的随机数
+					randRect(smallarr,newrand);
+					smalldraw(context2);
+					randRect(store,oldrand);
+					
+					shop =1;//每次产生新的方块时，进行初始化
+					flag = false;
+
+				}
+				moveDown(store);
+
+			},500);
+		}
 	}
 	draw(store,context);
 }
-function moveLeft(arr1) {
+function moveLeft(arr) {
 	for(var i=0; i<10; i++) {
 		for(var j=0; j<21; j++) {
-			if(arr1[j][i] === 2) {
-				if(i === 0 || arr1[j][i-1] === 1) {
+			if(arr[j][i] === 2) {
+				if(i === 0 || arr[j][i-1] === 1) {
 					return false;
 				} else {
-					arr1[j][i] = 0;
-					arr1[j][i-1] = 2;
+					arr[j][i] = 0;
+					arr[j][i-1] = 2;
 				}
 			}
 		}
 	}
 }
-function moveRight(arr2) {
+function moveRight(arr) {
 	for(var i=9; i>=0; i--) {
 		for(var j=0; j<21; j++) {
-			if(arr2[j][i] === 2) {
-				if(i === 9 || arr2[j][i+1] === 1) {
+			if(arr[j][i] === 2) {
+				if(i === 9 || arr[j][i+1] === 1) {
 					return false;
 				} else {
-					arr2[j][i] = 0;
-					arr2[j][i+1] = 2;
+					arr[j][i] = 0;
+					arr[j][i+1] = 2;
 				}
 			}
 		}
