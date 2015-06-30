7d4272579e939e45c332d0f881609cc6d58d70e6
wylcpu <wylcpu@163.com>
2015-06-30 20:13:59 +0800

    change

--- 2048/script.js
+++ 2048/script.js
@@ -1,45 +1,64 @@
 var canvas = document.getElementById('canvas');
+var fs = document.querySelector('.fs');
+//取得绘制上下文
 var context = canvas.getContext('2d');
-//获取屏幕的宽度
+//获取设备的宽度
 var sWidth = window.screen.availWidth;
 var cWidth = sWidth;
 var sideWidth = 0.04 * sWidth;
 var cellWidth = 0.2 *sWidth;
 var game = [];
+var score = 0;
 var fontSize = sWidth/10;
+//当设备的宽度大于500时，就不在增大
 if(sWidth > 500) {
 	cWidth = 500;
 	cellWidth = 100;
 	sideWidth = 20;
 	fontSize = 48;
 }
+//设置画布的长宽高
 canvas.width = cWidth;
 canvas.height = cWidth;
+//取得对应小方格的坐标x值
 function getX(i,j) {
 	return (sideWidth + cellWidth) * j + sideWidth;
 }
+//取得对应小方格坐标Y值
 function getY(i,j) {
 	return (sideWidth + cellWidth) * i + sideWidth;
 }
+//让其居中显示
 $("#box").width(cWidth).css('margin','0 auto');
 
 
 //开始一个新的游戏
 function newGame() {
 	for(var i=0; i<4; i++) {
-	game.push([{x:0,y:0,num:0,state:-1},{x:0,y:0,num:0,state:-1},{x:0,y:0,num:0,state:-1},{x:0,y:0,num:0,state:-1}]);
+		game[i] = new Array(4);
+		for(var j=0; j<4; j++) {
+			game[i][j] = {x:0,y:0,num:0,state:-1};
+		}
+	// game.push([{x:0,y:0,num:0,state:-1},{x:0,y:0,num:0,state:-1},{x:0,y:0,num:0,state:-1},{x:0,y:0,num:0,state:-1}]);
 	}
+	//绘制最初的背景
 	clearBg(context);
 	randNum();
 	randNum();
 	move();
 }
 newGame();
+//用户交互，键盘控制
+
 function move() {
 	$(document).keydown(function(evt) {
+		//阻止默认事件，上下键控制滚动条
 		evt.preventDefault();
+		//判断格子中是否都已经有数据
 		if(hasFill()) {
+			//进一步判断数字是否能合并
 			if(!fillHorizontal() && !fillVertical()) {
+				//数字已经填满，且不能合并
 				gameOver();
 				return ;
 			}
@@ -47,24 +66,28 @@ function move() {
 		switch(evt.keyCode) {
 			//向左
 			case 37:
+			//判断向左还能移动吗，如果没有能合并的项，再按向左是不能产生反应的
 			if(canLeft()) {
 				moveLeft();
 			}
 			break;
 			//向上
 			case 38:
+			//同上
 			if(canUp()) {
 				moveUp();	
 			}
 			break;
 			//向右
 			case 39:
+			//同上
 			if(canRight()) {
 				moveRight();	
 			}
 			break;
 			//向下
 			case 40: 
+			//同上
 			if(canDown()) {
 				moveDown();	
 			}
@@ -72,6 +95,7 @@ function move() {
 		}	
 	})
 }
+//手机滑动效果
 var startx = 0,
 	starty = 0,
 	endx = 0,
@@ -104,6 +128,9 @@ document.addEventListener('touchend',function(event) {
   	}
 
 });
+//cxt 上下文环境
+//obj 小格子的属性值
+//newi,newj最新坐标轴
 function animate(cxt,obj,newi,newj) {
 var ioldX = obj.x;
 var ioldY = obj.y;	
@@ -113,17 +140,21 @@ var istate = obj.state;
 var ispeed = 2;
 var inow = 0;
 var inum = obj.num;
+//绘制
 var itimer = setInterval(function() {
-	if((ioldX < inewX + 1 && ioldX > inewX -1) && (ioldY < inewY + 1 && ioldY > inewY - 1)) {
+	//判断正在移动的数字方格是否移动到指定的位置了
+	if((ioldX < inewX + 2 && ioldX > inewX -2) && (ioldY < inewY + 2 && ioldY > inewY - 2)) {
 		clearInterval(itimer);
+		//这个方格的由移动的变成不可以移动的
 		if(istate == 1) {
 			obj.state = 0;
-		} else if(istate == 2) {
+		} else if(istate == 2) {//由能合并的变成不能合并的
 			obj.state =0;
-			bigAnimate(context,obj.x,obj.y,obj.num,bgColor(obj.num),textColor(obj.num),cellWidth,cellWidth,fontSize)
-
+			//移动到指定的位置进行动画特效
+			bigAnimate(context,obj.x,obj.y,obj.num,bgColor(obj.num),textColor(obj.num),cellWidth,cellWidth,fontSize);
 		}
 	}
+		//判断这两值是否相等，这个就是产生由快到慢的动画
 	if(ioldX != inewX) {
 		inow = (inewX-ioldX)/ispeed;
 		inow = inow > 0 ? Math.ceil(inow) : Math.floor(inow);
@@ -134,13 +165,14 @@ var itimer = setInterval(function() {
 		inow = inow > 0 ? Math.ceil(inow) : Math.floor(inow);
 		ioldY += inow;
 	}
+	//更新方格的x，y值
 	game[newi][newj].x = ioldX;
 	game[newi][newj].y = ioldY;
+	//进行一次
 	clearBg(cxt);
 	for(var i = 0; i<4;i++) {
 		for(var j= 0; j<4; j++) {
 			if(game[i][j].num) {
-
 				if(game[i][j].state == 0) {
 					drawText(cxt,getX(i,j),getY(i,j),game[i][j].num,bgColor(game[i][j].num),textColor(game[i][j].num),fontSize);
 				} else if(game[i][j].state == 1) {
@@ -152,7 +184,7 @@ var itimer = setInterval(function() {
 		}
 	}
 		
-	},30);
+	},40);
 }
 
 function moveLeft() {
@@ -194,9 +226,11 @@ function moveRight() {
 			}
 		}
 	}
-	for(var i=0; i<4; i++) {
+	for(var i=3; i>=0; i--) {
 		for(var j=0; j<4; j++) {
-			animate(context,game[i][j],i,j)
+			if(game[j][i].num){
+				animate(context,game[j][i],j,i);
+			}
 		}
 	}
 	setTimeout(function() {
@@ -222,7 +256,9 @@ function moveUp() {
 	}
 	for(var i=0; i<4; i++) {
 		for(var j=0; j<4; j++) {
-			animate(context,game[i][j],i,j)
+				if(game[i][j].num){
+					animate(context,game[i][j],i,j)
+				}
 		}
 	}
 	setTimeout(function() {
@@ -246,9 +282,11 @@ function moveDown() {
 			}
 		}
 	}
-	for(var i=0; i<4; i++) {
+	for(var i=3; i>=0; i--) {
 		for(var j=0; j<4; j++) {
-			animate(context,game[i][j],i,j);
+				if(game[i][j].num){
+					animate(context,game[i][j],i,j);
+				}
 		}
 	}
 	setTimeout(function() {
@@ -263,17 +301,22 @@ function toDown(i,j) {
 	}
 	noStone(i,j,k-1,j,k-1,i);
 }
-
+//判断一个格子向前移动时可能出现的情况
 function Stone(i,j,m,n,x,y,k,z) {
+	//判断前方是否有格子
 if(game[m][n].num) {
-	if(game[m][n].num == game[i][j].num) {
+	//判断前方格子值是相等的情况
+	if(game[m][n].num == game[i][j].num && game[m][n].state != 2) {
 		game[m][n].num *=2;
+		score += game[m][n].num;
+		fs.innerHTML = score;
 		game[m][n].state = 2;
 		game[m][n].x = getX(i,j);
 		game[m][n].y = getY(i,j); 
 		game[i][j].num = 0;
 		game[i][j].state = -1;
 	} else {
+		//前面有格子，这个格子不是挨在一起时
 		if(k != z) {
 			game[x][y].num = game[i][j].num;
 			game[x][y].x = getX(i,j);
@@ -285,6 +328,7 @@ if(game[m][n].num) {
 	}
 	return true;
 }
+//前方没有障碍格子
 return false;
 }
 function noStone(i,j,m,n,k,z) {
@@ -292,6 +336,7 @@ function noStone(i,j,m,n,k,z) {
 	game[m][n].x = getX(i,j);
 	game[m][n].y = getY(i,j);
 	game[m][n].state = 1;
+	//判断这个格子是否已经在边上不能移动了
 	if(k != z) {
 		game[i][j].num = 0;
 		game[i][j].state = -1;
@@ -359,8 +404,7 @@ function drawBg(cxt) {
 	}
 }
 
-
-
+//绘制文本
 function drawText(cxt,posX,posY,num,bgColor,textColor,fs) {
 	drawArc(cxt,posX,posY,cellWidth,cellWidth,10,bgColor);
 	cxt.save();
@@ -421,7 +465,7 @@ function randNum() {
 	textAnimate(context,getX(x,y),getY(x,y),num,bgColor(num),textColor(num),0,0,0);
 
 }
-//动画
+//产生随意数字时用到的动画
 function textAnimate(cxt,posX,posY,num,bgColor,textColor,width,height,fs) {
 	posX += cellWidth/2;
 	posY += cellWidth/2; 
@@ -451,8 +495,9 @@ var timer = setInterval(function() {
 		cxt.fillStyle = textColor;
 		cxt.fillText(num,width/2,height/2);
 		cxt.restore();
-	},20);
+	},30);
 }
+//合并数字时产生的动画
 function bigAnimate(cxt,posX,posY,num,bgColor,textColor,width,height,fontSize) {
 	var count = 4;
 	var timer = setInterval(function() {
@@ -484,7 +529,7 @@ function bigAnimate(cxt,posX,posY,num,bgColor,textColor,width,height,fontSize) {
 		cxt.fillText(num,width/2,height/2);
 		cxt.restore();
 		count--;
-	},40);
+	},35);
 }
 //用于判断是否棋盘已经满了
 function hasFill() {
@@ -497,6 +542,7 @@ function hasFill() {
 	}
 	return true;
 }
+//判断是否水平还有能合并的吗
 function fillHorizontal() {
 	for(var i=0; i<4; i++) {
 		for(var j=0; j<3; j++) {
@@ -507,6 +553,7 @@ function fillHorizontal() {
 	}
 	return false;
 }
+//判断垂直方向是否还有能合并的吗
 function fillVertical() {
 	for(var i=0; i<4; i++) {
 		for(var j=0; j<3; j++) {
@@ -521,6 +568,7 @@ function fillVertical() {
 function gameOver() {
 	alert("gameover!");
 }
+//判断是否还能向下移动
 function canDown() {
 	for(var i=0; i<4; i++) {
 		for(var j=0; j<3; j++) {
@@ -531,6 +579,7 @@ function canDown() {
 	}
 	return false;
 }
+//判断是否还能向上移动
 function canUp() {
 	for(var i=0; i<4; i++) {
 		for(var j=3; j>0; j--) {
@@ -541,6 +590,7 @@ function canUp() {
 	}
 	return false;
 }
+//判断是否还能向左移动吗
 function canLeft() {
 	for(var i=0; i<4; i++) {
 		for(var j=3; j>0; j--) {
@@ -551,6 +601,7 @@ function canLeft() {
 	}
 	return false;
 }
+//判断是否不能向右移动
 function canRight () {
 	for (var i=0; i<4; i++) {
 		for(var j=0; j<3; j++) {
